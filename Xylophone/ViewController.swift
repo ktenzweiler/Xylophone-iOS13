0659e1d803e424674ceebcfb0c242c3714cfd8b3
ktenzweiler <ktenzweiler@gmail.com>
2025-06-30 23:21:02 -0500

    added opacity delay and sound playing

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,29 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(note: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
-
